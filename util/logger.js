46ee24b568726c20cbf51c794caa98c0eaf700c8
Adam Elsodaney <adam.elso@gmail.com>
2017-03-21 10:18:41 +0000

    Add timestamp to log entries.

--- util/logger.js
+++ util/logger.js
@@ -1,9 +1,13 @@
 'use strict';
 
 exports.logError = function (errorMessage) {
-  console.log('[package-cache] [ERROR] ' + errorMessage);
+  var d = new Date();
+
+  console.log(d.toISOString() + ' [package-cache] [ERROR] ' + errorMessage);
 };
 
 exports.logInfo = function (message) {
-  console.log('[package-cache] [INFO] ' + message);
+  var d = new Date();
+
+  console.log(d.toISOString() + ' [package-cache] [INFO] ' + message);
 };
